857fafe5431e55491e33b4f4e75a9b34aaca9c15
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2026-03-28 20:50:48 +0900

    fix: update static file serving paths to use 'dist' directory

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 const port = 80;
 
-app.use(express.static('public'));
-app.use(express.static('public/images'));
+app.use(express.static('dist'));
+app.use(express.static('dist/assets'));
+app.use(express.static('dist/assets/images'));
 
 app.listen(port, () => {
 	console.log(
